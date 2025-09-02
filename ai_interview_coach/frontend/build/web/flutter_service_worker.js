0b57d8ff9342b2376be60cb888e6320924ef9496
LeetAnkit <134221882+LeetAnkit@users.noreply.github.com>
2025-09-02 23:09:41 +0530

    final commit

--- ai_interview_coach/frontend/build/web/flutter_service_worker.js
+++ ai_interview_coach/frontend/build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9a1deec828c80e6db27f51012da62af4
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "9a0d579202e7b92e153aac1b25e67787",
+"flutter_bootstrap.js": "0b5f21862e1ffa85d7e4b3595834be09",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
